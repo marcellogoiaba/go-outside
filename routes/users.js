65cb4a5de2cdcb4b34c540c828ed89678976b629
Marcelo Silva <marceloperes.dasilva@student.ncirl.ie>
2017-05-06 15:42:31 +0100

    added registration function, working

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 //Register router
 router.post('/register', (req, res, next)=>{
-  res.send('REGISTER');
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    country: req.body.country
+  });
+
+  User.addUser(newUser, (err, user)=>{
+    if(err){
+      res.json({success: false, msg: 'Failed to register user'});
+    }
+    else{
+      res.json({success: true, msg: 'User registered successfully'});
+    }
+  });
 });
 
 //Authenticate
-router.get('/authenticate', (req, res, next)=>{
+router.post('/authenticate', (req, res, next)=>{
   res.send('AUTHENTICATE');
 });
 
 //Profile
 router.get('/profile', (req, res, next)=>{
   res.send('PROFILE');
-});
-
-//Validate
-router.get('/validate', (req, res, next)=>{
-  res.send('VALIDATE');
 });
 
 module.exports = router;
